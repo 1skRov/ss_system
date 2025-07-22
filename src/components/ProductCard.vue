1d04d3e1194562c1deb45f0a0e03b7900ac88362
sarvar <sarvarrozikulov9@gmail.com>
2025-07-22 17:29:50 +0500

    add ProductCard component and integrate it into CatalogPage

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="w-44 rounded border border-gray-200 max-h-44 min-h-44 h-full overflow-hidden">
+    <div class="w-full h-21 border-0 border-b border-gray-200">
+      <img src="@/assets/images/product1.jpg" alt="product" class="w-full h-full object-cover"/>
+    </div>
+    <div class="px-2 py-1 flex-1">
+      <div class="flex flex-col justify-between">
+        <span class="text-base font-medium">Lorem ipsum dolor.</span>
+        <span class="text-sm text-gray-600">1234 ₸</span>
+        <button class="btn btn-secondary btn-sm w-full text-white font-semibold text-base">Добавить</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/pages/CatalogPage.vue
+++ src/pages/CatalogPage.vue
@@ -1,10 +1,15 @@
 <script setup>
-
+import ProductCard from "@/components/ProductCard.vue";
+const count = 50;
 </script>
 
 <template>
   <div class="w-full h-full flex flex-col">
-    <div class="content h-full w-full">Catalog content</div>
+    <div class="content h-full w-full">
+      <div class="grid grid-cols-4 gap-4" style="width: 65vw" >
+        <ProductCard v-for="c in count" :key="c"></ProductCard>
+      </div>
+    </div>
   </div>
 </template>
 
