f3959c849f86e49f958719e11877eb0986716a33
sarvar <sarvarrozikulov9@gmail.com>
2025-07-27 04:19:06 +0500

    update

--- src/pages/CatalogPage.vue
+++ src/pages/CatalogPage.vue
@@ -1,49 +1,33 @@
 <script setup>
-import { ref } from 'vue'
+import {ref} from 'vue'
 import ProductCard from "@/components/ProductCard.vue"
 
-const categories = [
-  "Овощи", "Фрукты", "Молочные", "Мясо", "Рыба", "Хлеб", "Напитки", "Сладости", "Бакалея"
-]
-const expanded = ref(false)
-const selected = ref(categories[0])
 const count = 50
-
-function selectCategory(cat) {
-  selected.value = cat
-  expanded.value = false
-}
 </script>
 
 <template>
   <div class="w-full h-full flex flex-col">
     <div class="content h-full w-full">
-      <div style="width: 65vw">
-        <div class="flex flex-wrap gap-2 bg-base-200 border border-base-300 rounded-lg mb-2">
-          <template v-if="!expanded">
-            <button
-                class="btn bg-base-300 border border-accent text-lg"
-                :class="{'btn-primary': true}"
-                @click="expanded = true"
-            >
-              {{ selected }}
-            </button>
-          </template>
-          <template v-else>
-            <button
-                v-for="cat in categories"
-                :key="cat"
-                class="btn bg-base-300 border border-accent text-lg"
-                :class="{'btn-secondary': selected === cat}"
-                @click="selectCategory(cat)"
-            >
-              {{ cat }}
-            </button>
-          </template>
-        </div>
-        <div class="grid grid-cols-3 gap-3">
-          <ProductCard v-for="c in count" :key="c"></ProductCard>
-        </div>
+      <form class="filter flex w-full justify-start items-center overflow-x-auto flex-nowrap pb-4">
+        <input class="btn" type="radio" name="category" aria-label="Овощи"/>
+        <input class="btn" type="radio" name="category" aria-label="Фрукты"/>
+        <input class="btn" type="radio" name="category" aria-label="Молочные"/>
+        <input class="btn" type="radio" name="category" aria-label="Мясо"/>
+        <input class="btn" type="radio" name="category" aria-label="Рыба"/>
+        <input class="btn" type="radio" name="category" aria-label="Хлеб"/>
+        <input class="btn" type="radio" name="category" aria-label="Крупы"/>
+        <input class="btn" type="radio" name="category" aria-label="Макароны"/>
+        <input class="btn" type="radio" name="category" aria-label="Замороженные"/>
+        <input class="btn" type="radio" name="category" aria-label="Консервы"/>
+        <input class="btn" type="radio" name="category" aria-label="Сладости"/>
+        <input class="btn" type="radio" name="category" aria-label="Напитки"/>
+        <input class="btn" type="radio" name="category" aria-label="Чай и кофе"/>
+        <input class="btn" type="radio" name="category" aria-label="Соусы"/>
+        <input class="btn" type="radio" name="category" aria-label="Орехи"/>
+        <input class="btn btn-square" type="reset" value="×"/>
+      </form>
+      <div class="grid grid-cols-4 gap-3">
+        <ProductCard v-for="c in count" :key="c"></ProductCard>
       </div>
     </div>
   </div>
