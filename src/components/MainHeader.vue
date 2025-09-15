42541e69d0ba4bc2ef55d56d2a72f8eae97668d9
SarVar <sarvarrozikulov9@gmail.com>
2025-09-16 00:39:33 +0500

    update header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,106 +1,153 @@
 <template>
-  <div class="border-b border-0 border-base-300 flex items-center px-2 py-1">
-    <div class="flex">
-      <label class="input input-sm" style="border-radius: 0.25rem 0 0 0.25rem">
-        <MagnifyingGLassIcon />
-        <input type="text" v-model="searchQuery" @keydown.enter="handleSearch"
-          placeholder="Введите код и нажмите Enter" />
-      </label>
-      <button class="btn btn-neutral btn-sm" style="border-radius: 0 0.25rem 0.25rem 0" @click="openModal">Поиск
+  <div class="flex items-center px-2 py-1 justify-between shadow-md">
+    <div @click="openModal">
+      <InputComponent
+        v-model="login"
+        placeholder="Нажмите для поиска"
+        input-type="text"
+      >
+        <template #icon>
+          <MagnifyingGLassIcon class="text-gray-400/70" />
+        </template>
+      </InputComponent>
+    </div>
+
+    <div
+      class="flex overflow-hidden border border-gray-200 rounded-lg py-1 mx-auto"
+    >
+      <button class="head-btn">
+        <router-link to="/table-page" class="flex items-center gap-3">
+          <ListIcon />
+          <span>Заказ</span>
+        </router-link>
+      </button>
+      <button class="head-btn">
+        <router-link to="/catalog" class="flex items-center gap-3">
+          <CatalogIcon />
+          <span>Каталог</span>
+        </router-link>
+      </button>
+      <button class="head-btn">
+        <router-link to="/deferred-products" class="flex items-center gap-3">
+          <BarsDownIcon />
+          <span>Отложки</span>
+        </router-link>
       </button>
     </div>
-    <ul class="menu menu-horizontal rounded-box p-0 mx-auto">
-      <li>
-        <router-link to="/table-page" class="flex items-center" active-class="bg-base-300 text-black font-medium">
-          <ListIcon />
-          <span class="text-lg ml-1">Таблица</span>
-        </router-link>
-      </li>
-      <li>
-        <router-link to="/catalog" class="flex items-center" active-class="bg-base-300 text-black font-medium">
-          <CatalogIcon />
-          <span class="text-lg ml-1">Каталог</span>
-        </router-link>
-      </li>
-      <li>
-        <router-link to="/deferred-products" class="flex items-center"
-          active-class="bg-base-300 text-black font-medium">
-          <Bars4Icon />
-          <span class="text-lg ml-1">Список отложки</span>
-        </router-link>
-      </li>
-    </ul>
-    <div class="dropdown dropdown-end">
-      <div tabindex="0" role="button" class="avatar">
-        <div class="w-11 rounded-full">
-          <img src="../assets/images/user.jpg" class="w-full h-full" alt="" />
+
+    <div class="relative inline-block">
+      <button
+        @click="dropdownOpen = !dropdownOpen"
+        class="relative z-10 flex items-center px-2 py-3 text-sm text-gray-600 bg-white border border-gray-200 rounded-lg"
+      >
+        <span class="mx-1">Jane Doe</span>
+        <ChevronDownicon />
+      </button>
+
+      <Transition
+        enter-active-class="transition ease-out duration-100"
+        enter-from-class="opacity-0 scale-90"
+        enter-to-class="opacity-100 scale-100"
+        leave-active-class="transition ease-in duration-100"
+        leave-from-class="opacity-100 scale-100"
+        leave-to-class="opacity-0 scale-90"
+      >
+        <div
+          v-if="dropdownOpen"
+          class="absolute right-0 z-20 w-56 py-2 mt-2 overflow-hidden origin-top-right bg-white rounded-lg shadow-xl border border-gray-200"
+        >
+          <div
+            class="flex items-center p-3 text-sm text-gray-800 gap-3 cursor-pointer hover:bg-gray-100"
+            @click="handleLogout"
+          >
+            <logout-icon />
+            <p class="text-base m-0">Выйти</p>
+          </div>
+
+          <div
+            class="flex items-center p-3 text-sm text-gray-800 gap-3 cursor-pointer hover:bg-gray-100"
+            @click="dropdownOpen = false"
+          >
+            <SettingIcon />
+            <p class="m-0 text-base">Настройки</p>
+          </div>
         </div>
-      </div>
-      <ul tabindex="0" class="dropdown-content z-[1] menu p-3 shadow bg-base-100 rounded-box w-52 mt-2 text-lg">
-        <li>
-          <a @click="handleLogout" class="flex items-center gap-2">
-            <logout-icon></logout-icon>
-            <span>Выйти</span>
-          </a>
-        </li>
-        <ThemeSwitcher />
-        <li>
-          <router-link to="/colors" class="flex items-center gap-2">
-            <circle-icon></circle-icon>
-            <span>Цвета</span>
-          </router-link>
-        </li>
-      </ul>
+      </Transition>
     </div>
   </div>
-  <dialog id="my_modal_4" class="modal w-full h-full" ref="modal">
-    <div class="modal-box search-modal-box">
-      <form method="dialog">
-        <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 text-4xl">✕</button>
-      </form>
-      <SearchDrawer @close="closeModal" />
-    </div>
-  </dialog>
-</template>
-<script setup>
-import CatalogIcon from "@/assets/icons/Catalog.vue";
-import ListIcon from "@/assets/icons/List.vue";
-import LogoutIcon from "@/assets/icons/LogoutIcon.vue";
-import CircleIcon from "@/assets/icons/CircleIcon.vue";
-import MagnifyingGLassIcon from "@/assets/icons/MagnifyingGLassIcon.vue";
-import SearchDrawer from "@/widjets/SearchDrawer.vue";
-import { ref } from "vue";
-import { useRouter } from "vue-router";
-import { useAuthStore } from "@/stores/authStore";
-import Bars4Icon from "../assets/icons/Bars4Icon.vue";
 
-const modal = ref(null);
-const router = useRouter();
-const authStore = useAuthStore();
+  <Transition
+    enter-active-class="transition duration-300 ease-out"
+    enter-from-class="opacity-0"
+    enter-to-class="opacity-100"
+    leave-active-class="transition duration-150 ease-in"
+    leave-from-class="opacity-100"
+    leave-to-class="opacity-0"
+  >
+    <div
+      v-if="modalOpen"
+      class="fixed inset-0 z-10 overflow-y-auto bg-black/50 bg-opacity-50"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div
+        class="flex items-center justify-center min-h-screen min-w-screen p-2"
+      >
+        <SearchDrawer @close="closeModal" />
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/authStore'
+
+import SearchDrawer from '@/widjets/SearchDrawer.vue'
+import InputComponent from '@/components/UiComponents/InputComponent.vue'
+
+import CatalogIcon from '@/assets/icons/Catalog.vue'
+import ListIcon from '@/assets/icons/List.vue'
+import ChevronDownicon from '@/assets/icons/chevronDownicon.vue'
+import LogoutIcon from '@/assets/icons/LogoutIcon.vue'
+import MagnifyingGLassIcon from '@/assets/icons/MagnifyingGLassIcon.vue'
+import SettingIcon from '@/assets/icons/SettingIcon.vue'
+import BarsDownIcon from '@/assets/icons/BarsArrowDownIcon.vue'
+import XCircleIcon from '@/assets/icons/XCircleIcon.vue'
+
+const router = useRouter()
+const authStore = useAuthStore()
+const login = ref('')
+const dropdownOpen = ref(false)
+const modalOpen = ref(false)
 
 function handleLogout() {
-  authStore.logout();
-  router.push('/login');
+  authStore.logout()
+  router.push('/login')
+  dropdownOpen.value = false
 }
+
 function closeModal() {
-  if (modal.value) {
-    modal.value.close();
-  }
+  modalOpen.value = false
 }
+
 function openModal() {
-  if (modal.value) {
-    modal.value.showModal();
-  }
+  modalOpen.value = true
 }
 </script>
+
 <style scoped lang="scss">
-.search-modal-box {
-  width: 98vw !important;
-  height: 98vh !important;
-  max-width: 98vw !important;
-  max-height: 98vh !important;
-  padding: 0 !important;
+.head-btn {
   display: flex;
-  flex-direction: column;
+  align-items: center;
+  padding: 8px 16px;
+  font-size: 1rem;
+  font-weight: 500;
+  color: black;
+  transition:
+    background-color 0.2s,
+    color 0.2s;
 }
 </style>
--- src/widjets/SearchDrawer.vue
+++ src/widjets/SearchDrawer.vue
@@ -1,39 +1,39 @@
 <script setup>
-import { ref, computed, onMounted } from "vue";
-import LoginKeyboard from '@/components/LoginKeyboard.vue';
-import { useCatalogStore } from '@/stores/catalogStore';
-import ProductCard from "@/components/ProductCard.vue";
-const catalogStore = useCatalogStore();
-const products = computed(() => catalogStore.products);
-const isLoadingProducts = computed(() => catalogStore.isLoadingProducts);
-const selectedCategory = ref(null);
+import { ref, computed, onMounted } from 'vue'
+import LoginKeyboard from '@/components/LoginKeyboard.vue'
+import { useCatalogStore } from '@/stores/catalogStore'
+import ProductCard from '@/components/ProductCard.vue'
+const catalogStore = useCatalogStore()
+const products = computed(() => catalogStore.products)
+const isLoadingProducts = computed(() => catalogStore.isLoadingProducts)
+const selectedCategory = ref(null)
 
 async function selectCategory(category) {
-  selectedCategory.value = category._id;
-  await catalogStore.fetchProducts(category._id);
+  selectedCategory.value = category._id
+  await catalogStore.fetchProducts(category._id)
 }
 onMounted(async () => {
-  await catalogStore.fetchCategories();
+  await catalogStore.fetchCategories()
   if (catalogStore.categories.length > 0) {
-    selectCategory(catalogStore.categories[0]);
+    selectCategory(catalogStore.categories[0])
   }
-});
+})
 
-const emit = defineEmits(['close']);
+const emit = defineEmits(['close'])
 function handleClose() {
-  emit('close');
+  emit('close')
 }
 
 function handleKeyPress(key) {
   if (key === 'BACKSPACE') {
-    catalogStore.setSearchTerm(catalogStore.searchTerm.slice(0, -1));
+    catalogStore.setSearchTerm(catalogStore.searchTerm.slice(0, -1))
   } else {
-    catalogStore.setSearchTerm(catalogStore.searchTerm + key);
+    catalogStore.setSearchTerm(catalogStore.searchTerm + key)
   }
 }
 
 function handleClear() {
-  catalogStore.setSearchTerm('');
+  catalogStore.setSearchTerm('')
 }
 </script>
 
@@ -42,14 +42,8 @@ function handleClear() {
     <div class="w-full px-2 py-1.5">
       <input type="text" placeholder="Поиск..." class="input input-sm w-4/5" />
     </div>
-    <!-- <div class="filter flex w-full justify-start items-center overflow-x-auto flex-nowrap px-2 pb-2 gap-2">
-      <button v-for="category in catalogStore.categories" :key="category._id" class="btn btn-sm"
-        style="font-size: 16px;" :class="{ 'btn-active': selectedCategory === category.title }"
-        @click="selectCategory(category)">
-        {{ category.title }}
-      </button>
-    </div> -->
-    <LoginKeyboard @key-press="handleKeyPress" @clear="handleClear" @close="handleClose" />
+    <button @click="handleClose" class="text-white">close</button>
+    <LoginKeyboard @key-press="handleKeyPress" @clear="handleClear" />
   </div>
 </template>
 
